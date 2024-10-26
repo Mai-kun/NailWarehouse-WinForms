c90fcb6872a2ef2d448e44a1665f4d363185c9cb
Mai-kun <amaz1ngcreature10@mail.ru>
2024-10-26 19:45:03 +0300

    NuGet ProductManager Создан

--- NailWarehouse.ProductManager/ProductsManager.cs
+++ NailWarehouse.ProductManager/ProductsManager.cs
@@ -1,4 +1,6 @@
-﻿using NailWarehouse.Contracts;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using NailWarehouse.Contracts;
 using NailWarehouse.Contracts.Models;
 using NailWarehouse.ProductManager.Models;
 
@@ -7,34 +9,63 @@ namespace NailWarehouse.ProductManager
     /// <inheritdoc cref="IProductManager"/>
     public class ProductsManager : IProductManager
     {
-        private IProductStorage productStorage;
+        private readonly IProductStorage productStorage;
+        private readonly ILogger logger;
+        private const string TimeLoggerTemplate = "{0} выполнился за {1} мс";
 
-        public ProductsManager(IProductStorage productStorage)
+        public ProductsManager(IProductStorage productStorage, ILogger logger)
         {
             this.productStorage = productStorage;
+            this.logger = logger;
         }
 
         /// <inheritdoc cref="IProductManager.AddAsync(Product)"/>
         public async Task<Product> AddAsync(Product product)
         {
+            var stopwatch = Stopwatch.StartNew();
             var result = await productStorage.AddAsync(product);
+            stopwatch.Stop();
+
+            logger.LogDebug(string.Format(TimeLoggerTemplate, nameof(AddAsync), stopwatch.ElapsedMilliseconds));
             return result;
         }
 
         /// <inheritdoc cref="IProductManager.DeleteAsync(Guid)"/>
         public async Task<bool> DeleteAsync(Guid id)
         {
+            var stopwatch = Stopwatch.StartNew();
             var result = await productStorage.DeleteAsync(id);
+            if (result)
+            {
+                logger.LogDebug($"Продукт {id} удален");
+            }
+            stopwatch.Stop();
+
+            logger.LogDebug(string.Format(TimeLoggerTemplate, nameof(DeleteAsync), stopwatch.ElapsedMilliseconds));
             return result;
         }
 
         /// <inheritdoc cref="IProductManager.EditAsync(Product)"/>
         public Task EditAsync(Product product)
-            => productStorage.EditAsync(product);
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var result = productStorage.EditAsync(product);
+            stopwatch.Stop();
+
+            logger.LogDebug(string.Format(TimeLoggerTemplate, nameof(EditAsync), stopwatch.ElapsedMilliseconds));
+            return result;
+        }
 
         /// <inheritdoc cref="IProductManager.GetAllAsync"/>
         public Task<IReadOnlyCollection<Product>> GetAllAsync()
-            => productStorage.GetAllAsync();
+        {
+            var stopwatch = Stopwatch.StartNew();
+            var result = productStorage.GetAllAsync();
+            stopwatch.Stop();
+
+            logger.LogDebug(string.Format(TimeLoggerTemplate, nameof(GetAllAsync), stopwatch.ElapsedMilliseconds));
+            return result;
+        }
 
         /// <inheritdoc cref="IProductManager.GetStatsAsync"/>
         public async Task<IProductStats> GetStatsAsync()
--- NailWarehouse/Program.cs
+++ NailWarehouse/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Logging;
 using NailWarehouse.ProductManager;
 using NailWarehouse.Storage.Memory;
 
@@ -14,8 +15,11 @@ namespace NailWarehouse
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            using var factory = LoggerFactory.Create(builder => builder.AddDebug());
+            var logger = factory.CreateLogger("NailWarehouse.ProductManager");
+
             var storage = new MemoryProductStorage();
-            var manager = new ProductsManager(storage);
+            var manager = new ProductsManager(storage, logger);
             Application.Run(new MainForm(manager));
         }
     }
