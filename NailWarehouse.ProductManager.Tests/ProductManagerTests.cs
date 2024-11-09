93582898152fb704a6fbe1ea2ede89f9d001944d
Mai-kun <googleavto@rambler.ru>
2024-11-09 14:09:34 +0300

    Начал писать тесты

---
+++ NailWarehouse.ProductManager.Tests/ProductManagerTests.cs
@@ -0,0 +1,58 @@
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NailWarehouse.Contracts;
+using NailWarehouse.Contracts.Models;
+using Xunit;
+
+namespace NailWarehouse.ProductManager.Tests
+{
+    public class ProductManagerTests
+    {
+        private readonly IProductManager productManager;
+        private readonly Mock<IProductStorage> productStorageMock;
+        private readonly Mock<ILogger> loggerMock;
+
+        public ProductManagerTests()
+        {
+            productStorageMock = new Mock<IProductStorage>();
+            loggerMock = new Mock<ILogger>();
+            loggerMock.Setup(x => x.Log(LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.IsAny<It.IsAnyType>(),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()));
+
+
+            productManager = new ProductsManager(productStorageMock.Object, loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task AddAsyncShouldWork()
+        {
+            // Arrange
+            var model = new Product()
+            {
+                Id = Guid.NewGuid(),
+                Name = "qwe",
+                Size = 2,
+                Material = Materials.Copper.ToString(),
+                Quantity = 11,
+                MinimumQuantity = 1,
+                Price = 10,
+            };
+
+            productStorageMock.Setup(x => x.AddAsync(It.IsAny<Product>()))
+                .ReturnsAsync(model);
+
+            // Act
+            var result = await productManager.AddAsync(model);
+
+            // Assert
+            result.Should().NotBeNull().And.Be(model);
+            productStorageMock.Verify(x => x.AddAsync(It.Is<Product>(y => y.Id == model.Id)), Times.Once);
+            productStorageMock.VerifyNoOtherCalls();
+            loggerMock.Verify(x => x.LogInformation(It.IsAny<string?>(), It.IsAny<object[]>()), Times.Exactly(2));
+        }
+    }
+}
---
+++ NailWarehouse.Storage.Memory.Tests/MemoryProductStorageTests.cs
@@ -0,0 +1,81 @@
+﻿using System.Diagnostics;
+using FluentAssertions;
+using NailWarehouse.Contracts;
+using NailWarehouse.Contracts.Models;
+using Xunit;
+
+namespace NailWarehouse.Storage.Memory.Tests;
+
+/// <summary>
+/// Тесты для <see cref="MemoryProductStorage"/>
+/// </summary>
+public class MemoryProductStorageTests
+{
+    private readonly IProductStorage productStorage;
+
+    public MemoryProductStorageTests()
+    {
+        productStorage = new MemoryProductStorage();
+    }
+
+    /// <summary>
+    /// При пустом списке нет ошибок
+    /// </summary>
+    [Fact]
+    public async Task GetAllAsyncShouldNotThrow()
+    {
+        // Assert
+        Func<Task> act = productStorage.Awaiting(x => x.GetAllAsync());
+
+        // Assert
+        await act.Should().NotThrowAsync();
+    }
+
+    /// <summary>
+    /// Получение пустого списка
+    /// </summary>
+    [Fact]
+    public async Task GetAllAsyncShouldReturnEmpty()
+    {
+        // Assert
+        var result = await productStorage.GetAllAsync();
+
+        // Assert
+        result.Should()
+            .NotBeNull()
+            .And.BeEmpty();
+    }
+
+    [Fact]
+    public async Task AddAsyncShouldReturnValue()
+    {
+        // Arrange
+        var model = new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "qwe",
+            Size = 2,
+            Material = Materials.Copper.ToString(),
+            Quantity = 11,
+            MinimumQuantity = 1,
+            Price = 10,
+        };
+
+        // Assert
+        var result = await productStorage.AddAsync(model);
+
+        // Assert
+        result.Should()
+            .NotBeNull()
+            .And.BeEquivalentTo(new Product()
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Size = model.Size,
+                Material = model.Material,
+                Quantity = model.Quantity,
+                MinimumQuantity = model.MinimumQuantity,
+                Price = model.Price,
+            });
+    }
+}
