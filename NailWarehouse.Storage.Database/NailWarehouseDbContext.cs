e81df59a30b5996735afad70cdda36f5e7c48bc1
Mai-kun <amaz1ngcreature10@mail.ru>
2024-11-21 17:09:07 +0300

    Вернул пропавшие файлы

---
+++ NailWarehouse.Storage.Database/NailWarehouseDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NailWarehouse.Contracts.Models;
+
+namespace NailWarehouse.Storage.Database
+{
+    public class NailWarehouseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=DataGridView;Integrated Security=False;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
